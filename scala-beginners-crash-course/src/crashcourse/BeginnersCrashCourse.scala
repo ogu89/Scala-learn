f42a28c1a5e065563cbac7b32069134d44a3ffec
ogu89 <kawakami.a@ctw.inc>
2024-05-22 15:47:44 +0900

    feat: set up project

---
+++ scala-beginners-crash-course/src/crashcourse/BeginnersCrashCourse.scala
@@ -0,0 +1,17 @@
+package crashcourse
+
+object BeginnersCrashCourse extends App{
+  var var1 = 1;
+  val var2 = 2;
+
+  val bool: Boolean = true
+  val chr: Char = 'a'
+  var str: String = "String"
+  val int: Int = 123
+  val shrt: Short = 123
+  val lng: Long =  123
+  val lng2 = 123L
+  val flt: Float = 123.0f
+  val dbl: Double = 123.0
+
+}
